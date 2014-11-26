2bbfaa6a0e77b8b14b9fcdfcf40b60ca62a4e818
johnathanleuthold <johnathanleuthold@users.noreply.github.com>
2014-11-25 19:05:57 -0800

    Added comments to SessionsController

--- cst316users/app/controllers/sessions_controller.rb
+++ cst316users/app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
+#Sessions controller for managing persistent user information (user database ID and username)
 class SessionsController < ApplicationController
   before_filter :authenticate_user, :except => [:index, :login, :login_attempt, :logout]
   before_filter :save_login_state, :only => [:index, :login, :login_attempt]
+  #The following four are not yet fleshed out but act as method containers for their respective views.
   def home
 #Home Page
   end
@@ -13,6 +15,8 @@ class SessionsController < ApplicationController
   def login
 #Login Form
   end
+  #login_attempt method calls the authenticate method for the User model and if a User is returned
+  #welcomes the user back to the website, else it will inform them of an invalid password or username entry.
   def login_attempt
     authorized_user = User.authenticate(params[:username_or_email],params[:login_password])
     if authorized_user
@@ -25,6 +29,7 @@ class SessionsController < ApplicationController
       render "login"
     end
   end
+  #logout method simply sets the current session type user_id to nil and redircts the user to the login form.
   def logout
     session[:user_id] = nil
     redirect_to :action => 'login'
