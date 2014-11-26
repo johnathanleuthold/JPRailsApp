b81581811e95f4e5d356881b4c62b6316422f5b1
johnathanleuthold <johnathanleuthold@users.noreply.github.com>
2014-11-25 19:54:53 -0800

    Added comments to UserController

--- cst316users/app/controllers/user_controller.rb
+++ cst316users/app/controllers/user_controller.rb
@@ -1,7 +1,11 @@
+#User controller class for user creation.
 class UserController < ApplicationController
+#Creates a new user and places it in the @user class variable.
   def new
     @user = User.new
   end
+#create saves a new user to the database (unless validation fails or information is missing) and returns
+#"you signed up successfully", or, "Form is invalid" depending upon input.
   def create
     @user = User.new(params.require(:user).permit(:username, :password, :fname, :lname, :salt, :email, :password_confirmation))
     if @user.save
