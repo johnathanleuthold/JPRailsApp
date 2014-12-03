0d8712139fe6025353ae597ac608c3d80aa0ea2b
johnathanleuthold <johnathanleuthold@users.noreply.github.com>
2014-12-03 12:06:39 -0800

    Commented Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,18 @@
+###########################################################################################################################
+#Author: Johnathan Leuthold
+#Date: 11-26-2014
+#Modifications: 
+#Description: The Sessions controller class holds temporary user attributes across multiple views with a session object.
+###########################################################################################################################
+
 #Sessions controller for managing persistent user information (user database ID and username)
 class SessionsController < ApplicationController
   before_filter :authenticate_user, :except => [:index, :login, :login_attempt, :logout]
   before_filter :save_login_state, :only => [:index, :login, :login_attempt]
+  
+  ###########################################################################################################################
   #The following four are not yet fleshed out but act as method containers for their respective views.
+  ###########################################################################################################################
   def home
 #Home Page
   end
@@ -15,8 +25,11 @@ class SessionsController < ApplicationController
   def login
 #Login Form
   end
+  
+  ###########################################################################################################################
   #login_attempt method calls the authenticate method for the User model and if a User is returned
   #welcomes the user back to the website, else it will inform them of an invalid password or username entry.
+  ###########################################################################################################################
   def login_attempt
     authorized_user = User.authenticate(params[:username_or_email],params[:login_password])
     if authorized_user
@@ -25,12 +38,17 @@ class SessionsController < ApplicationController
       redirect_to(:action => 'home')
     else
       flash.now[:notice] = "Invalid Username or Password"
+      #Render login regenerates the login view in the current page.
       render "login"
     end
   end
+  
+  ###########################################################################################################################
   #logout method simply sets the current session type user_id to nil and redircts the user to the login form.
+  ###########################################################################################################################
   def logout
     session[:user_id] = nil
+    #Redirect actually changes pages vs render, which does not change the page.
     redirect_to :action => 'login'
   end
 end
