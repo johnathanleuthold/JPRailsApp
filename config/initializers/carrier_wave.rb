63ba963a3e63d827020d35d842b5187a9f063548
chedder00 <chedder00@gmail.com>
2014-12-25 01:28:08 +0000

    Relocated image upload button

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,10 +2,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
-      :provider => 'AWS',
-      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region => 'us-west-2'
+      :region                => 'us-west-2'
     }
     config.fog_directory = ENV['S3_BUCKET_NAME']
   end
